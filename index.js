b858e304c6691ecf16412915d0af037426d216e6
Leonardo Silva <coelho_ll@hotmail.com>
2019-11-25 19:01:08 -0700

    fixing bug

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
     express()
     .use(express.static(path.join(__dirname, '/public')))
     .use(bodyParser.urlencoded({ extended: true }))
-    .set('views', path.join(__dirname, 'views'))
+    .set('/views', path.join(__dirname, '/views'))
     .set('view engine', 'ejs')
    
 
@@ -58,7 +58,9 @@ const PORT = process.env.PORT || 5000
 		console.error(err);
 		res.send("Error " + err);
 	    }
-	})
+    })
+    
+
 
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
